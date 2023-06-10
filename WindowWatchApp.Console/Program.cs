c5dae80b0166709852b17c346ed668e8dc041b58
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2023-06-10 12:40:08 +0200

    Refactor console app

--- WindowWatchApp.Console/Program.cs
+++ WindowWatchApp.Console/Program.cs
@@ -1,45 +1,43 @@
-﻿namespace WindowWatchApp.Console
+﻿namespace WindowWatchApp.Console;
+
+using System;
+using WindowWatchApp.Common.Windows;
+
+public static class Program
 {
-    using System.Diagnostics;
-    using System.Runtime.InteropServices;
-    using System;
-    using WindowWatchApp.Common.Windows;
-
-    class Program
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    public static async Task Main(string[] args)
     {
-        static async Task Main(string[] args)
+        try
         {
-            Task.Run(RunStuff);
-            Console.ReadLine();
-        }
+            var activityTracker = new WindowsActivityTracker();
 
-        public static async Task RunStuff()
-        {
-            try
+            // If the user is inactive for more than 3 minutes, stop tracking.
+            var trackingTimeout = TimeSpan.FromMinutes(3);
+            var trackingService = new TrackingService(activityTracker, trackingTimeout);
+
+            // Start tracking at 10 second intervals.
+            trackingService.StartTracking(TimeSpan.FromSeconds(10));
+
+            while (true)
             {
-                var activityTracker = new WindowsActivityTracker();
+                await Task.Delay(1000);
+                Console.Clear();
+                await Console.Out.WriteLineAsync("Tracked Applications:");
 
-                var trackingTimeout = TimeSpan.FromMinutes(3);
-                var trackingService = new TrackingService(activityTracker, trackingTimeout);
-
-                trackingService.StartTracking(TimeSpan.FromSeconds(10));
-
-                while (true)
+                foreach (var trackedApp in trackingService.TrackedTime)
                 {
-                    await Task.Delay(1000);
-                    Console.Clear();
-                    await Console.Out.WriteLineAsync("Tracked Applications:");
-
-                    foreach (var trackedApp in trackingService.TrackedTime)
-                    {
-                        await Console.Out.WriteLineAsync($"{trackedApp.Key}: {trackedApp.Value.TotalSeconds}s");
-                    }
+                    await Console.Out.WriteLineAsync($"{trackedApp.Key}: {trackedApp.Value.TotalSeconds}s");
                 }
             }
-            catch (Exception ex)
-            {
-                await Console.Out.WriteLineAsync(ex.Message);
-            }
+        }
+        catch (Exception ex)
+        {
+            await Console.Out.WriteLineAsync(ex.Message);
         }
     }
 }
