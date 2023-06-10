6b6f929665bcfdd9d332bfebfc7eb1baf3182955
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2023-06-10 12:25:43 +0200

    Refactor service into common project

--- WindowWatchApp.Common/TrackingService.cs
+++ WindowWatchApp.Common/TrackingService.cs
@@ -2,14 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Timers;
     using WindowWatchApp.Common;
-    using WindowWatchApp.Common.Windows;
 
     public class TrackingService
     {
@@ -29,8 +23,6 @@
         {
             this.ActivityTracker = activityTracker;
             this.ActivityTimeout = activityTimeout;
-            //this.ActivityTimer.Elapsed += ActivityTimer_Elapsed;
-            //this.ActivityTimer.AutoReset = false; // Only trigger once until reset
         }
 
         public void StartTracking(TimeSpan trackingInterval)
@@ -45,22 +37,6 @@
         {
             this.TrackTimer.Stop();
         }
-
-        //private void ActivityTracker_ActivityOccurred(object sender, EventArgs e)
-        //{
-        //    // Reset the timer
-        //    this.ActivityTimer.Stop();
-        //    this.ActivityTimer.Start();
-
-        //    // Set IsActive to true
-        //    this.IsActive = true;
-        //}
-
-        //private void ActivityTimer_Elapsed(object sender, ElapsedEventArgs e)
-        //{
-        //    // No activity occurred within the timeout period, set IsActive to false
-        //    this.IsActive = false;
-        //}
 
         private void TrackingTimerElapsed(object sender, ElapsedEventArgs e)
         {
