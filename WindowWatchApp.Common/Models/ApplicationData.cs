917071d1868247dbfb0bdf7b09548a1f09eb786f
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2023-06-11 15:17:51 +0200

    Add property change

--- WindowWatchApp.Common/Models/ApplicationData.cs
+++ WindowWatchApp.Common/Models/ApplicationData.cs
@@ -1,8 +1,32 @@
-﻿namespace WindowWatchApp.Common.Models;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
-public class ApplicationData
+namespace WindowWatchApp.Common.Models;
+
+public class ApplicationData : INotifyPropertyChanged
 {
+    private TimeSpan trackedTime;
+
     public string ProcessName { get; set; }
 
-    public TimeSpan TrackedTime { get; set; }
+    public TimeSpan TrackedTime
+    {
+        get { return trackedTime; }
+
+        set
+        {
+            if (trackedTime != value)
+            {
+                trackedTime = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
